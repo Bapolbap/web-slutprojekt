966e301500730640dc8a1ad55ea920baf0ac0fc4
Fru1ty <pablo.jofre34@gmail.com>
2021-06-02 18:16:08 +0200

    added several pages, linked them

--- app.js
+++ app.js
@@ -8,10 +8,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(ClientDir));
 
-console.log(ClientDir);
+app.set('views', ClientDir + '\\ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render(ClientDir + "ejs\\index.ejs")
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/gallery', (req, res) => {
+    res.render('gallery');
 });
 
 app.listen(port, () => console.log(`panic in port ${port}!`))
