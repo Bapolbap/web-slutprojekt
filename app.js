396896326d56f224216fc4c83947b629ef57d1ed
Fru1ty <pablo.jofre34@gmail.com>
2021-06-02 17:52:47 +0200

    skapat en basic server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+const ClientDir = __dirname;
+
+
+
+console.log(__dirname);
